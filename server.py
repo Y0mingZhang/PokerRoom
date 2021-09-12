f84c5f9596ee3c4d4c562fb0096f7b51f0956a72
Yiming Zhang <yimingz0@uchicago.edu>
2021-09-12 07:07:00 +0000

    eventlet server

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ log = logging.getLogger("werkzeug")
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
-async_mode = None
+async_mode = 'eventlet'
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
